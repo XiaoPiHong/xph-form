c30986e5a1cc8ae71f336fee3e5733c7bb8204a9
zhouzhenhong <xiaopihong0325@qq.com>
2024-02-21 14:24:09 +0800

    修改

--- dist/src/react18-form/src/hooks/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useFormItem = void 0;
-var useFormItem_1 = require("./useFormItem");
-Object.defineProperty(exports, "useFormItem", { enumerable: true, get: function () { return __importDefault(useFormItem_1).default; } });
