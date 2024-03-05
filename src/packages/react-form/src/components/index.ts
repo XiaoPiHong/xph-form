42bce301b1c22498a04ef220dab450dc92db0113
zhouzhenhong <xiaopihong0325@qq.com>
2024-03-05 16:00:40 +0800

    修改

--- src/packages/react-form/src/components/index.ts
+++ src/packages/react-form/src/components/index.ts
@@ -1,5 +1,19 @@
 import React from "react";
-import { Input, Select, InputNumber } from "antd";
+import {
+  Input,
+  Select,
+  InputNumber,
+  Tree,
+  TreeSelect,
+  Transfer,
+  Switch,
+  Button,
+  Radio,
+  Checkbox,
+  Cascader,
+  TimePicker,
+  DatePicker,
+} from "antd";
 import { TComponentPropsMap } from "../types";
 
 const componentMap = new Map<keyof TComponentPropsMap, React.FC<any>>();
@@ -7,5 +21,15 @@ const componentMap = new Map<keyof TComponentPropsMap, React.FC<any>>();
 componentMap.set("Input", Input);
 componentMap.set("Select", Select);
 componentMap.set("InputNumber", InputNumber);
+componentMap.set("Tree", Tree);
+componentMap.set("TreeSelect", TreeSelect);
+componentMap.set("Transfer", Transfer);
+componentMap.set("Switch", Switch);
+componentMap.set("Button", Button);
+componentMap.set("Radio", Radio);
+componentMap.set("Checkbox", Checkbox);
+componentMap.set("Cascader", Cascader);
+componentMap.set("TimePicker", TimePicker);
+componentMap.set("DatePicker", DatePicker);
 
 export { componentMap };
--- src/packages/react-form/src/types/component.ts
+++ src/packages/react-form/src/types/component.ts
@@ -1,7 +1,31 @@
-import { SelectProps, InputProps, InputNumberProps } from "antd";
+import {
+  SelectProps,
+  InputProps,
+  InputNumberProps,
+  TreeProps,
+  TreeSelectProps,
+  TransferProps,
+  SwitchProps,
+  ButtonProps,
+  RadioProps,
+  CheckboxProps,
+  CascaderProps,
+  TimePickerProps,
+  DatePickerProps,
+} from "antd";
 
 export type TComponentPropsMap = {
   Select: SelectProps;
   Input: InputProps;
   InputNumber: InputNumberProps;
+  Tree: TreeProps;
+  TreeSelect: TreeSelectProps;
+  Transfer: TransferProps;
+  Switch: SwitchProps;
+  Button: ButtonProps;
+  Radio: RadioProps;
+  Checkbox: CheckboxProps;
+  Cascader: CascaderProps;
+  TimePicker: TimePickerProps;
+  DatePicker: DatePickerProps;
 };
