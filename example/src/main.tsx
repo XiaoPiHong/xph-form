c8fe6eb06bacb6b51cb6635cc71930da9ff41ec4
zhouzhenhong <xiaopihong0325@qq.com>
2024-04-19 10:28:29 +0800

    需要的依赖改为前置依赖

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import Test from "./Test.tsx";
 import "./index.css";
+import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
-  <App />
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: "orange",
+      },
+    }}
+  >
+    <App />
+  </ConfigProvider>
   // </React.StrictMode>
 );
