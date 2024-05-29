e3e0297728cf710f64b180681b3d4652388a777a
zhouzhenhong <xiaopihong0325@qq.com>
2024-05-29 17:40:14 +0800

    fix: 修改

--- example/src/TableApp.tsx
+++ example/src/TableApp.tsx
@@ -23,20 +23,24 @@ const ReactApp: React.FC = () => {
       apiPagination: false,
       api: async (params) => {
         console.log(params);
-        return [
-          {
-            key: "1",
-            name: "胡彦斌",
-            age: 32,
-            address: "西湖区湖底公园1号",
-          },
-          {
-            key: "2",
-            name: "胡彦祖",
-            age: 42,
-            address: "西湖区湖底公园1号",
-          },
-        ];
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve([
+              {
+                key: "1",
+                name: "胡彦斌",
+                age: 32,
+                address: "西湖区湖底公园1号",
+              },
+              {
+                key: "2",
+                name: "胡彦祖",
+                age: 42,
+                address: "西湖区湖底公园1号",
+              },
+            ]);
+          }, 2000);
+        });
       },
     },
     searchForm: {
