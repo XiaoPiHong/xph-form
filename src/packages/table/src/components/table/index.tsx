d216797f57823f4fcb4acf5e0b96ddfea01c373c
zhouzhenhong <xiaopihong0325@qq.com>
2024-05-29 14:25:01 +0800

    fix: 修改

---
+++ src/packages/table/src/components/table/index.tsx
@@ -0,0 +1,59 @@
+import { Table as ATable } from "antd";
+import React, { memo, Ref } from "react";
+import { TTableProps } from "../../types";
+import { useTableColumns, usePagination, useTable } from "../../hooks";
+import { isObject } from "lodash-es";
+import { IXphFormActionType } from "@xph-form/form";
+
+const Table = ({
+  tableProps,
+  searchFormRef,
+}: {
+  tableProps: TTableProps;
+  searchFormRef: Ref<IXphFormActionType>;
+}) => {
+  const { columns } = useTableColumns(tableProps);
+  const { pagination, lastPaginationState } = usePagination(tableProps);
+  const { table, firstGetTableData, getTableData, onAllChange } = useTable(
+    tableProps,
+    {
+      pagination,
+      lastPaginationState,
+    },
+    searchFormRef
+  );
+
+  const getTableBindProps = () => {
+    const {
+      autoRequest,
+      api,
+      formatDataSource,
+      apiPagination,
+      columns,
+      ...rest
+    } = tableProps.table!;
+    return rest;
+  };
+
+  /** 首次请求 */
+  firstGetTableData();
+
+  return (
+    <ATable
+      {...getTableBindProps()}
+      columns={columns}
+      /** pagination可以是个boolean */
+      pagination={
+        isObject(pagination.model)
+          ? { ...pagination.model, disabled: table.model.loading }
+          : pagination.model
+      }
+      loading={table.model.loading}
+      dataSource={table.model.dataSource}
+      /** 分页、排序、筛选变化时触发 */
+      onChange={onAllChange}
+    />
+  );
+};
+
+export default memo(Table);
--- src/packages/table/src/table.tsx
+++ src/packages/table/src/table.tsx
@@ -1,44 +1,12 @@
-import React, { forwardRef, useImperativeHandle, useEffect } from "react";
-import { Table as ATable } from "antd";
+import React, { forwardRef, useImperativeHandle } from "react";
 import { TTableProps, TRefTable } from "./types";
 import SearchForm from "./components/searchForm";
-import {
-  useSearchForm,
-  useTableColumns,
-  useTableProps,
-  usePagination,
-  useTable,
-} from "./hooks";
-import { isObject } from "lodash-es";
+import { useSearchForm, useTableProps } from "./hooks";
+import Table from "./components/table";
 
-const Table: TRefTable = forwardRef((props: TTableProps, ref) => {
+const XphTable: TRefTable = forwardRef((props: TTableProps, ref) => {
   const { tableProps } = useTableProps(props);
   const { searchFormRef, searchFormProps } = useSearchForm(tableProps);
-  const { columns } = useTableColumns(tableProps);
-  const { pagination, lastPaginationState } = usePagination(tableProps);
-  const { table, firstGetTableData, getTableData, onAllChange } = useTable(
-    tableProps,
-    {
-      pagination,
-      lastPaginationState,
-    },
-    searchFormRef
-  );
-
-  const getTableBindProps = () => {
-    const {
-      autoRequest,
-      api,
-      formatDataSource,
-      apiPagination,
-      columns,
-      ...rest
-    } = tableProps.table!;
-    return rest;
-  };
-
-  /** 首次请求 */
-  firstGetTableData();
 
   useImperativeHandle(ref, () => ({}));
   return (
@@ -47,23 +15,10 @@ const Table: TRefTable = forwardRef((props: TTableProps, ref) => {
         <SearchForm ref={searchFormRef} {...searchFormProps} />
       </div>
       <div>
-        <ATable
-          {...getTableBindProps()}
-          columns={columns}
-          /** pagination可以是个boolean */
-          pagination={
-            isObject(pagination.model)
-              ? { ...pagination.model, disabled: table.model.loading }
-              : pagination.model
-          }
-          loading={table.model.loading}
-          dataSource={table.model.dataSource}
-          /** 分页、排序、筛选变化时触发 */
-          onChange={onAllChange}
-        />
+        <Table tableProps={tableProps} searchFormRef={searchFormRef} />
       </div>
     </div>
   );
 });
 
-export default Table;
+export default XphTable;
