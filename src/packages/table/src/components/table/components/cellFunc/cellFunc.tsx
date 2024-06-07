a203fd7fca8abea55a733bb0d4e9df43f1171ad7
zhouzhenhong <xiaopihong0325@qq.com>
2024-06-07 17:01:11 +0800

    fix: 修改

--- src/packages/table/src/components/table/components/cellFunc/cellFunc.tsx
+++ src/packages/table/src/components/table/components/cellFunc/cellFunc.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { CellComponentMap } from "./components";
+import { ICellFuncProps } from "./types";
 
 /** 最底层的组件 */
 const Main = () => {
   return <div></div>;
 };
 
-/** CellFunc需循环处理组件，这个是一个递归组件 */
-const CellFunc = (props) => {
+/** CellFunc需循环处理组件(这个是一个递归组件) */
+const CellFunc = <T,>(props: ICellFuncProps<T>) => {
   return <div></div>;
 };
 
--- src/packages/table/src/components/table/components/cellFunc/types/cellFunc.ts
+++ src/packages/table/src/components/table/components/cellFunc/types/cellFunc.ts
@@ -1,12 +1,22 @@
 export type TCellComponentType = "link";
 
+/** cellFunc中每一项对应的组件属性映射  */
 export interface ICellComponentPropsMap {
   link: {
     url: string;
   };
 }
 
+/** cellFunc中每一项得配置  */
 export interface ICellProps<T extends TCellComponentType = TCellComponentType> {
   component: T;
   componentProps?: ICellComponentPropsMap[T];
+}
+
+export interface ICellFuncProps<
+  T,
+  U extends TCellComponentType = TCellComponentType
+> {
+  dslConfig: ICellProps<U>[];
+  renderPrams: [value: any, record: T, index: number];
 }
